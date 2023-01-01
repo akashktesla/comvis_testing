7b1515b2a1fab807d12e486a27cd056af0317375
akashktesla <akashktesla@gmail.com>
2023-01-02 04:03:33 +0530

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![ allow(warnings) ]
+
+use opencv::{highgui, prelude::*, videoio, Result};
+
+fn main() -> Result<()> {
+    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;
+    highgui::named_window("window", highgui::WINDOW_FULLSCREEN)?;
+    let mut frame = Mat::default();
+    loop{
+        cam.read(&mut frame)?;
+        highgui::imshow("window", &frame)?;
+        let key = highgui::wait_key(1)?;
+        if key == 113{
+            break;
+        } }
+
+    Ok(())
 }
